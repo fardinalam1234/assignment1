a6b46e47ba236b1b0d55e0223175379a502b2d91
fardinalam1234 <90734889+fardinalam1234@users.noreply.github.com>
2023-03-15 22:24:47 -0400

    Update main.js

--- SpamDetectorClient/js/main.js
+++ SpamDetectorClient/js/main.js
@@ -1,4 +1,20 @@
 // TODO: onload function should retrieve the data needed to populate the UI
 
-
-
+window.onload = function() {
+  // Make a GET request to the API endpoint that returns the spam analysis results
+  fetch("http://localhost:8080/spamDetector-1.0/api/spam/results")
+    .then(response => response.json())
+    .then(data => {
+      const tableBody = document.querySelector("tbody");
+      data.forEach(result => {
+        const row = tableBody.insertRow();
+        const filenameCell = row.insertCell();
+        const categorizationCell = row.insertCell();
+        const actualCategoryCell = row.insertCell();
+        filenameCell.textContent = result.filename;
+        categorizationCell.textContent = result.categorization;
+        actualCategoryCell.textContent = result.actualCategory;
+      });
+    })
+    .catch(error => console.error(error));
+}
